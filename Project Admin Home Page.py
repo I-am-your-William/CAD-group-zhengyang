5f212a71a6ea20d45f7dfcbe37f07814bacdda57
kochiling <112928080+kochiling@users.noreply.github.com>
2023-11-10 00:57:23 +0800

    Add files via upload

--- Project Admin Home Page.py
+++ Project Admin Home Page.py
@@ -4,7 +4,8 @@ from tkinter import ttk
 import  tkinter.messagebox
 from PIL import ImageTk, Image
 import sqlite3
-
+from contextlib import closing
+import webbrowser
 
 LARGEFONT = ("Verdana", 35)
 
@@ -20,7 +21,7 @@ class Application(tk.Tk):
 
         self.frames = {}
 
-        for F in (HomePage, UploadPage, ManagePage, ClinicRequestPage, ViewAllClinicPage ) :
+        for F in (HomePage, UploadPage, ManagePage, ClinicRequestPage, ViewAllClinicPage, ViewTipsPage ) :
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -66,13 +67,16 @@ class HomePage(tk.Frame):
         self.left_side_panel.place(height=550, width=300, x=55, y=100)
 
         view_clinic_btn = Button (self.left_side_panel , text = " View All Clinic ", font=(20),background="white",command=lambda: controller.show_frame(ViewAllClinicPage))
-        view_clinic_btn.place(height=70,width=200, x=50, y=50)
+        view_clinic_btn.place(height=70,width=200, x=50, y=40)
 
         upload_tip_btn = Button (self.left_side_panel, text = " Upload Tips ", font=(20),background="white", command=lambda: controller.show_frame(UploadPage))
-        upload_tip_btn.place(height=70,width=200, x=50, y=200)
+        upload_tip_btn.place(height=70,width=200, x=50, y=180)
 
         manage_tip_btn = Button (self.left_side_panel , text = " Manage Tips ", font=(20),background="white", command=lambda: controller.show_frame(ManagePage))
-        manage_tip_btn.place(height=70,width=200, x=50, y=350)
+        manage_tip_btn.place(height=70,width=200, x=50, y=330)
+
+        view_all_tips_btn = Button (self.left_side_panel , text = " View All Tips ", font=(20),background="white", command=lambda: controller.show_frame(ViewTipsPage))
+        view_all_tips_btn.place(height=70,width=200, x=50, y=470)
 
         #right side panel
         self.right_side_panel = Frame(home_page , background="light grey")
@@ -115,27 +119,26 @@ class UploadPage(tk.Frame):
         self.title_label = Label(self.frame2, text = "Tilte : ", font=(20),background = "light grey")
         self.title_label.place(x=50, y=30)
         self.title_entry = Entry(self.frame2)
-        self.title_entry.place(height=30,width=200, x=150, y=30)
+        self.title_entry.place(height=30,width=500, x=150, y=30)
 
         self.content_label = Label(self.frame2, text = "Content Description : ", font=(20),background = "light grey")
         self.content_label.place(x=50, y=80)
         self.content_entry = Text(self.frame2)
-        self.content_entry.place(height=90,width=310, x=50, y=120)
+        self.content_entry.place(height=90,width=600, x=50, y=120)
 
         self.link_label = Label(self.frame2, text = "Link : ", font=(20),background = "light grey")
         self.link_label.place(x=50, y=250)
         self.link_entry = Entry(self.frame2)
-        self.link_entry.place(height=30,width=200, x=150, y=250)
+        self.link_entry.place(height=30,width=500, x=150, y=250)
 
         upload_btn = Button(self.frame2, text = "Upload",background="white", font=(20),command=self.upload)
-        upload_btn.place(height=50, width=100,x=50, y=300)
+        upload_btn.place(height=50, width=100,x=50, y=400)
 
     def upload(self):
         # Extract the content from the Text widget
         self.title_entry_submit = self.title_entry.get()
         self.content_entry_submit = self.content_entry.get("1.0", "end-1c")  # Retrieve the entire content
         self.link_entry_submit = self.link_entry.get()
-
 
         # Making sure all three fields are populated before submitting to the database
         if (
@@ -145,23 +148,32 @@ class UploadPage(tk.Frame):
         ):
             tkinter.messagebox.showinfo("Submit", "All fields need to be populated, nothing saved")
         else:
-            self.conn = sqlite3.connect("CAD_Database.db")  # Connect to the database
+            # Use the existing database file path
+            db_path = "C:/Users/choon/Documents/Chi Ling/BCSCUN/Software Engineering/CAD_Database.db"
+
+            self.conn = sqlite3.connect(db_path)  # Connect to the existing database
             self.c = self.conn.cursor()  # Create a cursor
-            self.c.execute(
-                """CREATE TABLE IF NOT EXISTS TIPS(
+
+            # Check if the "TIPS" table exists, and if not, create it
+            self.c.execute("""
+                CREATE TABLE IF NOT EXISTS TIPS (
                     Post_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                    TITLE VARCHAR (100) NOT NULL, 
-                    CONTENT VARCHAR (1000)  NOT NULL,
-                    LINK VARCHAR (100) NOT NULL)"""
-            )
+                    TITLE VARCHAR(100) NOT NULL,
+                    CONTENT VARCHAR(1000) NOT NULL,
+                    LINK VARCHAR(100) NOT NULL
+                )
+            """)
+
+            # Continue with inserting the data
             self.c.execute(
-                "INSERT INTO TIPS ( TITLE, CONTENT, LINK) VALUES(?,?,?)",
-                ( self.title_entry_submit, self.content_entry_submit, self.link_entry_submit),
+                "INSERT INTO TIPS (TITLE, CONTENT, LINK) VALUES(?,?,?)",
+                (self.title_entry_submit, self.content_entry_submit, self.link_entry_submit),
             )
-            new_post_id = self.c.lastrowid 
-            self.conn.commit()  # This line physically enters the data into the database.
+            new_post_id = self.c.lastrowid
+            self.conn.commit()
             self.conn.close()
-            tkinter.messagebox.showinfo("Data saved")
+
+            tkinter.messagebox.showinfo("Data saved", "Data saved successfully!")
             self.clearTextInput()
 
             self.controller.frames[ManagePage].update_treeview()
@@ -258,11 +270,22 @@ class ManagePage(tk.Frame):
         tree_tips.tag_configure('odd', background="light grey")
         tree_tips.tag_configure('even', background="white") 
         
-        
+        db_path = "C:/Users/choon/Documents/Chi Ling/BCSCUN/Software Engineering/CAD_Database.db"
 
-        
-        self.conn = sqlite3.connect("CAD_Database.db")
-        self.c = self.conn.cursor()
+        self.conn = sqlite3.connect(db_path)  # Connect to the existing database
+        self.c = self.conn.cursor()  # Create a cursor
+
+        # Check if the "TIPS" table exists, and if not, create it
+        self.c.execute("""
+            CREATE TABLE IF NOT EXISTS TIPS (
+                Post_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                TITLE VARCHAR(100) NOT NULL,
+                CONTENT VARCHAR(1000) NOT NULL,
+                LINK VARCHAR(100) NOT NULL
+            )
+        """)
+
+        # Continue with executing the SELECT statement
         self.c.execute("SELECT * FROM TIPS")
         displayrecord = self.c.fetchall()
 
@@ -286,8 +309,21 @@ class ManagePage(tk.Frame):
             tree_tips.delete(*tree_tips.get_children())  # Clear the existing data
 
         # Fetch and insert the latest data from the database
-        self.conn = sqlite3.connect("CAD_Database.db")
-        self.c = self.conn.cursor()
+         
+        db_path = "C:/Users/choon/Documents/Chi Ling/BCSCUN/Software Engineering/CAD_Database.db"
+
+        self.conn = sqlite3.connect(db_path)  # Connect to the existing database
+        self.c = self.conn.cursor()  # Create a cursor
+
+        # Check if the "TIPS" table exists, and if not, create it
+        self.c.execute("""
+            CREATE TABLE IF NOT EXISTS TIPS (
+                Post_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                TITLE VARCHAR(100) NOT NULL,
+                CONTENT VARCHAR(1000) NOT NULL,
+                LINK VARCHAR(100) NOT NULL
+            )
+        """)
         self.c.execute("SELECT * FROM TIPS")
         displayrecord = self.c.fetchall()
 
@@ -299,7 +335,8 @@ class ManagePage(tk.Frame):
             else:
                 tree_tips.insert('', 'end', iid=count, values=(all[0], all[1], all[2], all[3]), tags=('odd',))
             count += 1
-
+        
+    
     def save(self):
         self.content_entry_submit = self.content_entry.get("1.0", "end-1c") 
         if self.title_entry.get() == "" or self.content_entry_submit == "" or self.link_entry.get() == "":
@@ -383,6 +420,140 @@ class ClinicRequestPage(tk.Frame):
         
         ############# 
 
+        self.frame2 = Frame(self.request_frame , background = "light grey" )
+        self.frame2.place(height=550, width=900, x=55, y=100)
+
+        request_canvas = Canvas(self.frame2, background="white")
+        request_canvas.place(height=550, width=900, x=0, y=0)
+
+        scrollbar = Scrollbar(self.frame2, orient=VERTICAL, command=request_canvas.yview)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        request_canvas.configure(yscrollcommand=scrollbar.set)
+        request_canvas.bind('<Configure>', lambda e: request_canvas.configure(scrollregion=request_canvas.bbox("all")))
+
+        frame_inner = Frame(request_canvas, background="lightgrey",height=550, width=900)
+        request_canvas.create_window((0, 0), window=frame_inner, anchor="nw")
+
+        self.load_request(frame_inner)
+
+    def load_request(self, frame_inner):
+        db_path = "C:/Users/choon/Documents/Chi Ling/BCSCUN/Software Engineering/CAD_Database.db"
+        self.conn = sqlite3.connect(db_path)  # Connect to the existing database
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM ClinicInformation WHERE status=0")
+        displayrecord = self.c.fetchall()
+
+        i = 0
+        for _ in displayrecord:
+            request_frame = Frame(frame_inner, background="white", highlightbackground="black", highlightthickness=1)
+            request_frame.place(height=200, width=800, x=50, y=10 + i * 210)
+            request_name = Label(request_frame, text=displayrecord[i][1], font=("Helvetica", 16))
+            request_name.pack(pady=10)
+            request_location = Label(request_frame, text="Location : " + displayrecord[i][4], font=("Helvetica", 12))
+            request_location.pack(pady=5)
+            
+            request_button = Button(request_frame, text="More Information", command=lambda i=i: self.accept_request(user_id=displayrecord[i][0]))
+            request_button.pack(pady=5)
+
+            i += 1
+
+        self.conn.close()
+
+    def accept_request(self, user_id):
+        # Fetch the information for the selected user_id
+        db_path = "C:/Users/choon/Documents/Chi Ling/BCSCUN/Software Engineering/CAD_Database.db"
+        self.conn = sqlite3.connect(db_path)
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM ClinicInformation WHERE user_id=?", (user_id,))
+        selected_record = self.c.fetchone()
+        self.conn.close()
+
+        # Create a new frame to display additional information
+        more_info_frame = tk.Frame(self.request_frame, background="white")
+        more_info_frame.place(height=640, width=1000, x=0, y=60)
+
+        left_panel1 = Frame(more_info_frame, background="white", highlightbackground="black", highlightthickness=1)
+        left_panel1.place(height=500, width=550, x=30, y=100)    
+
+        right_panel1 = Frame(more_info_frame, background="white", highlightbackground="black", highlightthickness=1)
+        right_panel1.place(height=250, width=350, x=610, y=100)
+
+        back_button = tk.Button(more_info_frame, text="Back to Clinic Request", command=lambda: more_info_frame.destroy())
+        back_button.place(height=30, width=130, x=30, y=30)
+
+        # Define the callback function
+        def callback(url):
+            webbrowser.open_new(url)
+
+        #Right Panel
+
+        first_label = tk.Label(right_panel1, text="1. Click on this Link below ", font=("Helvetica", 10),background="white")
+        first_label.place(height=30, width=150, x=20, y=20)
+        link = tk.Label(right_panel1, text="https://meritsmmc.moh.gov.my/search", font=("Helvetica", 10),background="white",fg="blue")
+        link.place(height=30, width=250, x=20, y=40)
+        link.bind("<Button-1>", lambda e:callback("https://meritsmmc.moh.gov.my/search"))
+        link.config(cursor="hand2")
+        second_label = tk.Label(right_panel1, text="2. Search the clinic name on the website ", font=("Helvetica", 10),background="white")
+        second_label.place(height=30, width=240, x=20, y=70)
+        third_label = tk.Label(right_panel1, text=("3. Check and verify the founder name \n" + "and registration number of the clinic " ), font=("Helvetica", 10),background="white")
+        third_label.place(height=30, width=215, x=20, y=120)
+        fourth_label = tk.Label(right_panel1, text="4. If true click approve else click decline", font=("Helvetica", 10),background="white")
+        fourth_label.place(height=30, width=235, x=20, y=170)
+
+        
+        accept_button = tk.Button(more_info_frame, text="Approve", command=lambda: self.destroy_and_accept(more_info_frame, user_id), background="green", fg="white",font=(20))
+        accept_button.place(height=50, width=350, x=610, y=380)
+        decline_button = tk.Button(more_info_frame, text="Decline", background="red", fg="white",font=(20))
+        decline_button.place(height=50, width=350, x=610, y=450)
+        
+        # Display information in the new frame
+        clinic_name = tk.Label(more_info_frame, text=selected_record[1], font=("Helvetica", 25),background="white")
+        clinic_name.place(height=30, width=300, x=350, y=30)
+
+        clinic_info = tk.Label(left_panel1, text="Clinic Information", font=("Helvetica", 20),background="white")
+        clinic_info.pack(pady=20)
+        clinic_operationTime = tk.Label(left_panel1, text="Operation Time: " + selected_record[2], font=("Helvetica", 12),background="white")
+        clinic_operationTime.pack(pady=1,anchor="w")
+        clinic_phoneNumber = tk.Label(left_panel1, text="Phone Number: " + selected_record[5], font=("Helvetica", 12),background="white")
+        clinic_phoneNumber.pack(pady=1,anchor="w")
+        clinic_location = tk.Label(left_panel1, text="Location: " + selected_record[4], font=("Helvetica", 12),background="white")
+        clinic_location.pack(pady=1,anchor="w")
+        clinic_codinates = tk.Label(left_panel1, text="Coordinate: " + selected_record[3], font=("Helvetica", 12),background="white")
+        clinic_codinates.pack(pady=1,anchor="w")
+        clinic_registration = tk.Label(left_panel1, text="Registration Number: " + selected_record[6], font=("Helvetica", 12),background="white")
+        clinic_registration.pack(pady=1,anchor="w")
+        clinic_description = tk.Label(left_panel1, text="Description: " + selected_record[7], font=("Helvetica", 12),background="white")
+        clinic_description.pack(pady=1,anchor="w")
+        founder_info = tk.Label(left_panel1, text="Founder Information", font=("Helvetica", 20),background="white")
+        founder_info.pack(pady=20)
+        founder_name = tk.Label(left_panel1, text="Founder Name: " + selected_record[8], font=("Helvetica", 12),background="white")
+        founder_name.pack(pady=1,anchor="w")
+        founder_email = tk.Label(left_panel1, text="Email: " + selected_record[10], font=("Helvetica", 12),background="white")
+        founder_email.pack(pady=1,anchor="w")
+        founder_phone = tk.Label(left_panel1, text="Contact: " + selected_record[9], font=("Helvetica", 12),background="white")
+        founder_phone.pack(pady=1,anchor="w")
+
+        
+
+    def destroy_and_accept(self, frame, user_id):
+        # Update the status of the request in the database
+        db_path = "C:/Users/choon/Documents/Chi Ling/BCSCUN/Software Engineering/CAD_Database.db"
+        self.conn = sqlite3.connect(db_path)
+        self.c = self.conn.cursor()
+        self.c.execute("UPDATE ClinicInformation SET status=1 WHERE user_id=?", (user_id,))
+        self.conn.commit()
+        self.conn.close()
+
+        # Update the treeview in ViewAllClinicPage
+        self.controller.frames[ViewAllClinicPage].update_treeview()
+
+        # Destroy the new frame
+        frame.destroy()
+
+
+        
+
 class ViewAllClinicPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -401,12 +572,85 @@ class ViewAllClinicPage(tk.Frame):
         back_btn = Button(self, text = "Back",background="white",command=lambda: controller.show_frame(HomePage))
         back_btn.place(height=30, width=100,x=800, y=10)
 
+        ####### Main frame #####
+        self.frame2 = Frame(self.clinicView_frame , background = "light grey" )
+        self.frame2.place(height=550, width=900, x=55, y=100)
+
+
+class ViewTipsPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.tipsView_frame = Frame(self, background="white")
+        self.tipsView_frame.place(height=700, width=1000)
+
+        #### Top Panel #######
+
+        self.top_frame1 = Frame(self.tipsView_frame , background="light grey")
+        self.top_frame1.place(height=60, width=1000, x=1, y=1)
+
+        TipsView_label  = Label(self.top_frame1 ,  text="Additional Tips", font=("Helvetica", 24),background = "light grey")
+        TipsView_label .pack()
+
+        back_btn = Button(self, text = "Back",background="white",command=lambda: controller.show_frame(HomePage))
+        back_btn.place(height=30, width=100,x=800, y=10)
+
+
+        self.frame2 = Frame(self.tipsView_frame, background="light grey")
+        self.frame2.place(height=550, width=900, x=55, y=100)
+
+        tips_canvas = Canvas(self.frame2, background="white")
+        tips_canvas.place(height=550, width=900, x=0, y=0)
+
+        scrollbar = Scrollbar(self.frame2, orient=VERTICAL, command=tips_canvas.yview)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        tips_canvas.configure(yscrollcommand=scrollbar.set)
+        tips_canvas.bind('<Configure>', lambda e: tips_canvas.configure(scrollregion=tips_canvas.bbox("all")))
+
+        frame_inner = Frame(tips_canvas, background="lightgrey",height=550, width=900)
+        tips_canvas.create_window((0, 0), window=frame_inner, anchor="nw")
+
+        self.load_tips(frame_inner)
+
+            
+    def load_tips(self, frame_inner):
+                db_path = "C:/Users/choon/Documents/Chi Ling/BCSCUN/Software Engineering/CAD_Database.db"
+
+                self.conn = sqlite3.connect(db_path)  # Connect to the existing database
+                self.c = self.conn.cursor()
+                self.c.execute("SELECT * FROM TIPS")
+                displayrecord = self.c.fetchall()
+
+                i = 0
+                for TIPS in displayrecord:
+                    tips_frame = Frame(frame_inner, background="white", highlightbackground="black", highlightthickness=1)
+                    tips_frame.place(height=200, width=800, x=50, y=10 + i * 210)
+                    tips_label = Label(tips_frame, text=TIPS[1], font=("Helvetica", 16))
+                    tips_label.pack(pady=10)
+                    tips_description = Label(tips_frame, text=TIPS[2], font=("Helvetica", 12))
+                    tips_description.pack(pady=5)
+                    tips_link = Label(tips_frame, text=TIPS[3], font=("Helvetica", 10))
+                    tips_link.pack(pady=5)
+                    i += 1
+
+                self.conn.close()
+    
+
+        
+
+
+
+
+       
 
 if __name__ == "__main__":
-    app = Application()
-    app.geometry('1000x700')
-    app.resizable(True, True)
-    app.title('Call A Doctor')
-    app.mainloop()
+            app = Application()
+            app.geometry('1000x700')
+            app.resizable(True, True)
+            app.title('Call A Doctor')
+
+            app.mainloop()
+
 
 
